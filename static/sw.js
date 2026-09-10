9f799a630fa842b6e74121a3191c9c017ddc64b1
Hifju khatri <hifju001@gmail.com>
2026-09-10 15:19:14 +0530

    Add files via upload

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "algobot-v10-3-cache-20260908";
+const CACHE_NAME = "algobot-v11-mode-strategy-rr";
 const ASSETS = ["/login.html", "/dashboard.html", "/manifest.json"];
 
 self.addEventListener("install", (event) => {
